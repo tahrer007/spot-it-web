751d0c056a10c6c105a42769b955ed0113972b34
tahrer007 <tahrer_Ad@outlook.com>
2022-02-01 23:25:41 +0200

    add react map

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 
 import './App.css';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+
+
 
 function App() {
+
+  
+  const mapStyles = {
+    width: '100%',
+    height: '100%',
+  };
   return (
     <div className="App">
-      test !!
+      <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176}}
+        />
     </div>
   );
 }
---
+++ client/src/Map.js
@@ -0,0 +1,5 @@
+
+/*import { Map, GoogleApiWrapper } from 'google-maps-react';
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyApf...vCF1UFAo'
+  })(MapContainer);*/
