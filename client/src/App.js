62a3665cee3e08d64c857fad358297eb045a2823
tahrer007 <tahrer_Ad@outlook.com>
2022-02-02 16:55:35 +0200

    fix map bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,32 @@
 
 import './App.css';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React from 'react';
+import {
+  GoogleMap,
+ useLoadScript,
+  //Marker,
+ // InfoWindow,
+} from "@react-google-maps/api";
 
-
-
-
+const mapContainerStyle = {
+  height: "100vh",
+  width: "100vw",
+};
+const options = {
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const center = {
+  lat: 43.6532,
+  lng: -79.3832,
+};
 function App() {
+  return <div>
 
-  
-  const mapStyles = {
-    width: '100%',
-    height: '100%',
-  };
-  return (
-    <div className="App">
-      <Map
-          google={this.props.google}
-          zoom={8}
-          style={mapStyles}
-          initialCenter={{ lat: 47.444, lng: -122.176}}
-        />
-    </div>
-  );
+    <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center}>
+
+    </GoogleMap>
+  </div>
 }
 
 export default App;
--- client/src/Map.js
+++
@@ -1,5 +0,0 @@
-
-/*import { Map, GoogleApiWrapper } from 'google-maps-react';
-export default GoogleApiWrapper({
-    apiKey: 'AIzaSyApf...vCF1UFAo'
-  })(MapContainer);*/
