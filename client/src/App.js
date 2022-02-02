dc0f385778c17f0b2f6d152144e55739c9e9e02c
tahrer007 <tahrer_Ad@outlook.com>
2022-02-02 22:28:43 +0200

    create alert windws

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,36 @@
 import "./App.css";
-import mapStyles from "./mapStyles"
-import React ,{useState} from "react";
+import mapStyles from "./mapStyles";
+import "@reach/combobox/styles.css";
+import React, { useState ,useCallback ,useRef } from "react";
+import { formatRelative } from "date-fns";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
 
 import {
   GoogleMap,
   useLoadScript,
   Marker,
-  // InfoWindow,
+   InfoWindow,
 } from "@react-google-maps/api";
 
 const mapContainerStyle = {
-  height: "50vh",
-  width: "50vw",
+  height: "80vh",
+  width: "80vw",
 };
 const options = {
-  styles : mapStyles,
+  styles: mapStyles,
   disableDefaultUI: true,
   zoomControl: true,
+  clickableIcons: false
 };
 const center = {
   lat: 32.794044,
@@ -25,15 +39,13 @@ const center = {
 
 const libraries = ["places"];
 function App() {
-
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey:  "AIzaSyApfEJizBV1MmMpqHfTZiGKrQkvCF1UFAo",
+    googleMapsApiKey: "AIzaSyApfEJizBV1MmMpqHfTZiGKrQkvCF1UFAo",
     libraries,
   });
-  const [markers,setMarkers]=useState([]) ;
-  /*const onMapClick = React.useCallback((e) => {
-
-   
+  const [markers, setMarkers] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const onMapClick = useCallback((e) => {
     setMarkers((current) => [
       ...current,
       {
@@ -42,8 +54,13 @@ function App() {
         time: new Date(),
       },
     ]);
-    console.log(markers);
-  }, []);*/
+  }, []);
+
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+  
   if (loadError) return "error loading map";
   if (!isLoaded) return "loading maps.....";
   return (
@@ -52,37 +69,49 @@ function App() {
         mapContainerStyle={mapContainerStyle}
         zoom={13}
         center={center}
-        options ={options}
-        onClick={(event)=>{
-          console.log(event.latLng.lat(),event.latLng.lng())
-          setMarkers((current)=>[
-            ...current,
-            {
-              lat :event.latLng.lat(),
-              lng :event.latLng.lng(),
-              time:new Date()
-            }
-          ])
-        }}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
       >
-{markers.map((marker) => (
+        {markers.map((marker) => (
           <Marker
-          key ={marker.time.toISOString()}
+            key={marker.time.toISOString()}
             position={{ lat: marker.lat, lng: marker.lng }}
             icon={{
               url: `/Pumbaa.png`,
               origin: new window.google.maps.Point(0, 0),
               anchor: new window.google.maps.Point(15, 15),
-              scaledSize: new window.google.maps.Size(40, 40),
+              scaledSize: new window.google.maps.Size(30, 30),
+            }}
+            onClick={() => {
+              setSelected(marker);
             }}
           />
         ))}
- 
-
-        
+         {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>
+                <span role="img" aria-label="bear">
+                  🐗
+                </span>{" "}
+                Alert
+              </h2>
+              <p>Spotted {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
       </GoogleMap>
     </div>
   );
+}
+function Search (){
+  
 }
 
 export default App;
