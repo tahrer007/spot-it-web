7bd7a73d5ade5dccc9702ed8dd9d72129c1ca37d
tahrer007 <tahrer_Ad@outlook.com>
2022-02-02 21:12:10 +0200

    marker icon

--- client/src/App.js
+++ client/src/App.js
@@ -60,22 +60,24 @@ function App() {
             {
               lat :event.latLng.lat(),
               lng :event.latLng.lng(),
+              time:new Date()
             }
           ])
         }}
       >
-{/*{markers.map((marker) => (
+{markers.map((marker) => (
           <Marker
+          key ={marker.time.toISOString()}
             position={{ lat: marker.lat, lng: marker.lng }}
             icon={{
-             // url: `../public/pumba.png`,
+              url: `/Pumbaa.png`,
               origin: new window.google.maps.Point(0, 0),
               anchor: new window.google.maps.Point(15, 15),
-              scaledSize: new window.google.maps.Size(30, 30),
+              scaledSize: new window.google.maps.Size(40, 40),
             }}
           />
         ))}
- */}
+ 
 
         
       </GoogleMap>
