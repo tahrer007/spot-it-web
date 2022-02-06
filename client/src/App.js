d4b50f7f803ff7f932fa39922e7904e559103dfd
tahrer007 <tahrer_Ad@outlook.com>
2022-02-06 23:19:55 +0200

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-
+import Navbar from "./components/navbar/navbar"
 import Home from "./components/home/home";
 import Help from "./components/help/help";
 import About from "./components/about/about";
@@ -9,8 +9,10 @@ import ErrorPage from "./components/errorPage/errorPage";
 
 function App() {
   return (
-    <div>
+    <div className="appContainer">
+     
       <Router>
+           <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ client/src/components/navbar/navbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <header className="navbar">
+        <div className="navbarLeft">
+          <div className="navbarLogo"> logo</div>
+          <Link className="navbarLink navbarBtn" to="/">
+            Homepage
+          </Link>
+        </div>
+        <Link className="navbarLink navbarBtn" to="/about">
+          About
+        </Link>
+        <Link className="navbarLink navbarBtn" to="/help">
+          Help
+        </Link>
+      </header>
+    );
+  }
+}
