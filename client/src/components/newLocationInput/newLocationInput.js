ab034e0a6f68e5ee2f450ea432c96d70ab1baed4
tahrer007 <tahrer_Ad@outlook.com>
2022-02-07 14:22:36 +0200

    dta input basic logics

--- client/src/components/newLocationInput/newLocationInput.js
+++ client/src/components/newLocationInput/newLocationInput.js
@@ -1,11 +1,73 @@
-import React from "react";
-import "./newLocationInput.css"
-
+import React, { useState, useEffect } from "react";
+import "./newLocationInput.css";
 
 export default function NewLocationInput() {
+  const [howMany, setHowMany] = useState("");
+  const [details, setDetails] = useState("");
+
+  const handleRadioChange = (event) => {
+    setHowMany(event.target.value);
+    console.log(event.target.value);
+  };
+  const handleTextChange = (event) => {
+    setDetails(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("You clicked submit.");
+  };
+  const cancel = ()=>{
+    console.log("cancel")
+  }
   return (
     <div className="newLocationBox">
-          new location box    
+      <h2> Please add some details :</h2>
+      <form onSubmit={handleSubmit}>
+        <strong>how many boars did you spot? </strong>
+        <div className="radio">
+          <label>
+            <input
+              type="radio"
+              value="one"
+              onChange={handleRadioChange}
+              checked={howMany === "one"}
+            />
+            one
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="2-5"
+              onChange={handleRadioChange}
+              checked={howMany === "2-5"}
+            />
+            2-5
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="more than 5"
+              onChange={handleRadioChange}
+              checked={howMany === "more than 5"}
+            />
+            More than 5
+          </label>
+        </div>
+        <div className="extraDetails">
+          <strong>
+            {" "}
+            Extra details? <span>(optional)</span>{" "}
+          </strong>
+
+          <label>
+            <textarea value={details} onChange={handleTextChange} />
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+        <button type="reset" onClick={()=>cancel()}>cancel</button>
+      </form>
     </div>
   );
 }
