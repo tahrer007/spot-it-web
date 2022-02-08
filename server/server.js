444425b9daed5cd42afe44361901916dfab8420e
tahrer007 <tahrer_Ad@outlook.com>
2022-02-08 11:09:23 +0200

    make it better

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("./dataBase/mongoose");
+const  path = require('path');
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
@@ -27,6 +28,13 @@ const PORT = process.env.PORT || 5000;
   
   
 });*/
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on Port: ${PORT}`);
 });
