d56a91b0e90129ed8c8d807fc8bfb496bdf8276d
tahrer007 <tahrer_Ad@outlook.com>
2022-02-11 23:53:32 +0200

    home page information

--- client/src/components/homeText/homeText.js
+++ client/src/components/homeText/homeText.js
@@ -1,14 +1,16 @@
 import React from "react";
-import "./homeText.css"
-
+import "./homeText.css";
 
 export default function HomeText() {
   return (
     <div className="homeText">
-        <h3> do you wanna build a snow man ??? </h3>   
-        <button className="receiveNotifcations" onClick={()=>console.log("do you wanna build a snowman")}> yes</button>
-        
-             
+      <h3> How its work ? </h3>
+      <ol>
+        <li>Add the location to the map</li>
+        <li>Fill few information </li>
+        <li>Click send </li>
+      </ol>
+      <h3> Thats it.  every user posted , thanks to you </h3>
     </div>
   );
 }
