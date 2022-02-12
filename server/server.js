d26a8d3399d5f9c8c0549933f4d90ec5cad85953
tahrer007 <tahrer_Ad@outlook.com>
2022-02-12 23:44:08 +0200

    npm run build

--- server/server.js
+++ server/server.js
@@ -27,31 +27,12 @@ const publicVapidKey =
   "BBgUIHi4LfvctkcccjVg05kEzrY7o9tdsYwOi6feFCPdBfau8XvgF0OhhIxdUahH8YQslCkBepXUAxKoJatyiuM";
 const privateVapidKey = "73QfPzJ-24R4LXHWq5x1Hnr8FeMySDov1DujsEmlErc";
 
-webpush.setVapidDetails(
-  "mailto:test@test.com",
-  publicVapidKey,
-  privateVapidKey
-);
 
-// Subscribe Route
-app.post("/subscribe", (req, res) => {
-  // Get pushSubscription object
-  const subscription = req.body;
-  // Set static path
-app.use(express.static(path.join(__dirname, "client")));
 
-  // Send 201 - resource created
-  res.status(201).json({});
 
-  // Create payload
-  const payload = JSON.stringify({ title: "Push Test" });
 
-  // Pass object into sendNotification
-  webpush
-    .sendNotification(subscription, payload)
-    .catch((err) => console.error(err));
-});
 
+  
 const PORT = process.env.PORT || 5000;
 
 /*app.use(express.static(path.join(__dirname, "client")));
@@ -63,7 +44,7 @@ app.get("/*", (req, res) => {
 app.all("*", (req, res) => {
   res.sendFile(path.resolve(publicPath, "index.html"));
 });*/
-
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.listen(PORT, () => {
   console.log(`Server running on Port: ${PORT}`);
