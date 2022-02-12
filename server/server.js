dec51612e5f6ccd9d01b40310d0064cfd6075ee4
tahrer007 <tahrer_Ad@outlook.com>
2022-02-13 00:55:37 +0200

    fix bug

--- server/server.js
+++ server/server.js
@@ -10,30 +10,29 @@ const logger = require("morgan");
 const locationsRouter = require("./routers/locationsRouter");
 const usersRouter = require("./routers/usersRouter");
 
-app.use(bodyParser.urlencoded());
+const PORT = process.env.PORT || 5000;
+const publicPath = path.join(__dirname, "../..", "client/build");
+
+app.use(express.static(publicPath));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*", // allow to server to accept request from different origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // allow session cookie from browser to pass through
-  })
-);
+app.use(cors());
 
 app.use("/users", usersRouter);
 app.use("/locations", locationsRouter);
 
+app.all("*", (req, res) => {
+  res.sendFile(path.resolve(publicPath, "index.html"));
+});
+
+const publicPath = path.join(__dirname, "..", "client/build");
+app.listen(PORT, () => {
+  console.log(`Server running on Port: ${PORT}`);
+});
+
 /*const publicVapidKey =
   "BBgUIHi4LfvctkcccjVg05kEzrY7o9tdsYwOi6feFCPdBfau8XvgF0OhhIxdUahH8YQslCkBepXUAxKoJatyiuM";
 const privateVapidKey = "73QfPzJ-24R4LXHWq5x1Hnr8FeMySDov1DujsEmlErc";*/
-
-
-
-
-
-
-  
-const PORT = process.env.PORT || 5000;
 
 /*app.use(express.static(path.join(__dirname, "client")));
 
@@ -44,10 +43,3 @@ app.get("/*", (req, res) => {
 app.all("*", (req, res) => {
   res.sendFile(path.resolve(publicPath, "index.html"));
 });*/
-app.use(express.static('public'))
-
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-
-app.listen(PORT, () => {
-  console.log(`Server running on Port: ${PORT}`);
-});
