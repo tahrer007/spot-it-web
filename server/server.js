8d6e9daeea5d2ef11ffa0c6dd88ae419531d5bd8
tahrer007 <tahrer_Ad@outlook.com>
2022-02-13 21:09:09 +0200

    jason to deploy to heruko

--- server/server.js
+++ server/server.js
@@ -8,23 +8,25 @@ const cors = require("cors");
 const logger = require("morgan");
 const locationsRouter = require("./routers/locationsRouter");
 const usersRouter = require("./routers/usersRouter");
-
+const publicPath = path.join(__dirname, "../client/build");
 const PORT = process.env.PORT || 5000;
-//const publicPath = path.join(__dirname, "..", "client/build");
-//app.use(express.static(publicPath));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
 app.use("/users", usersRouter);
 app.use("/locations", locationsRouter);
+app.use(express.static(publicPath));
 
-if (process.env.NODE_ENV === "production") {
+/*if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
-}
+}*/
+
+app.use("*", (req, res) => {
+	res.send("this route is not exist");
+});
 
 app.listen(PORT, (error) => {
   if (error) return console.log(error);
