dfc9d46dfa5bdcbbc1d9f17bd42e0438813a4ebe
tahrer007 <tahrer_Ad@outlook.com>
2022-02-13 20:44:58 +0200

    fix bug heruko

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ require("./dataBase/mongoose");
 const path = require("path");
 const bodyParser = require("body-parser");
 const express = require("express");
-const webpush = require("web-push");
 const app = express();
 const cors = require("cors");
 const logger = require("morgan");
@@ -11,9 +10,8 @@ const locationsRouter = require("./routers/locationsRouter");
 const usersRouter = require("./routers/usersRouter");
 
 const PORT = process.env.PORT || 5000;
-const publicPath = path.join(__dirname, "..", "client/build");
-
-app.use(express.static(publicPath));
+//const publicPath = path.join(__dirname, "..", "client/build");
+//app.use(express.static(publicPath));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -21,24 +19,14 @@ app.use(cors());
 app.use("/users", usersRouter);
 app.use("/locations", locationsRouter);
 
-app.all("*", (req, res) => {
-  res.sendFile(path.resolve(publicPath, "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+}
 
-app.listen(PORT, () => {
+app.listen(PORT, (error) => {
+  if (error) return console.log(error);
   console.log(`Server running on Port: ${PORT}`);
 });
-
-/*const publicVapidKey =
-  "BBgUIHi4LfvctkcccjVg05kEzrY7o9tdsYwOi6feFCPdBfau8XvgF0OhhIxdUahH8YQslCkBepXUAxKoJatyiuM";
-const privateVapidKey = "73QfPzJ-24R4LXHWq5x1Hnr8FeMySDov1DujsEmlErc";*/
-
-/*app.use(express.static(path.join(__dirname, "client")));
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "index.html"));
-});
-
-app.all("*", (req, res) => {
-  res.sendFile(path.resolve(publicPath, "index.html"));
-});*/
