893b387c97dbe112c8077e4e2de92316003be59b
tahrer007 <tahrer_Ad@outlook.com>
2022-02-13 21:40:05 +0200

    re build

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,37 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
-//import { Redirect } from 'react-router-dom';
 
-export default  function Navbar () {
-  //const navigate = useNavigate();
+export default function Navbar() {
+  const handleClick = () => {
+    console.log("route to about page");
+  };
 
-   const handleClick = ()=> {
-   console.log("route to about page")
-  }
- 
-    return (
-      <header className="navbar">
-        <div className="navbarLeft">
-        <div className="navbarLogo" onClick={() =>  handleClick()  }>
-          </div>
-        </div>
-        <Link className="navbarLink navbarBtn" to="/Locations">
-          Locations
-        </Link>
-        <Link className="navbarLink navbarBtn" to="/Redneighborhoods">
-         Red-Neighborhoods
-        </Link>
-        <Link className="navbarLink navbarBtn" to="/about">
-          About
-        </Link>
-       {/* <Link className="navbarLink navbarBtn" to="/help">
+  return (
+    <header className="navbar">
+      <div className="navbarLeft">
+        <div className="navbarLogo" onClick={() => handleClick()}></div>
+      </div>
+      <Link className="navbarLink navbarBtn" to="/Locations">
+        Locations
+      </Link>
+      <Link className="navbarLink navbarBtn" to="/Redneighborhoods">
+        Red-Neighborhoods
+      </Link>
+      <Link className="navbarLink navbarBtn" to="/about">
+        About
+      </Link>
+      {/* <Link className="navbarLink navbarBtn" to="/help">
           Help
         </Link>
         <Link className="navbarLink navbarBtn" to="/login">
           Log in
   </Link>*/}
-      </header>
-    );
-  
+    </header>
+  );
 }
--- server/server.js
+++ server/server.js
@@ -10,19 +10,15 @@ const locationsRouter = require("./routers/locationsRouter");
 const usersRouter = require("./routers/usersRouter");
 const publicPath = path.join(__dirname, "../client/build");
 const PORT = process.env.PORT || 5000;
+app.use(express.static(publicPath));
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use(cors());
 app.use("/users", usersRouter);
 app.use("/locations", locationsRouter);
-app.use(express.static(publicPath));
 
-/*if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-  });
-}*/
+
 
 app.use("*", (req, res) => {
 	res.send("this route is not exist");
