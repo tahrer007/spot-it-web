f87743946f80e137583505f568dfd0eaff73e280
tahrer007 <tahrer_Ad@outlook.com>
2022-02-15 21:28:17 +0200

    change main font

--- client/src/components/homeText/homeText.js
+++ client/src/components/homeText/homeText.js
@@ -12,7 +12,7 @@ export default function HomeText() {
         </ol>
       </div>
 
-      <h3> Thats it . we are posted , thanks to you </h3>
+      <h3> Thats it . we are posted , thanks to you 😉 </h3>
     </div>
   );
 }
